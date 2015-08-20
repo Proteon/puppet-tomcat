4dbbfbfd0693dee01c28ac3ab4bae9e1373d49e8
Simon Smit <Simon.johannes.smit@gmail.com>
2015-08-20 13:31:39 +0200

    Making init script more robust (hopefully) and adding a clean-restart function

--- manifests/init.pp
+++ manifests/init.pp
@@ -24,72 +24,73 @@ class tomcat (
     $version = $tomcat::params::version,
     $start_on_boot = true,
 ) inherits tomcat::params {
-  include concat::setup
+    include concat::setup
 
-  package { "tomcat${version}": ensure => held, }
+    package { "tomcat${version}": ensure => held, }
 
-  package { ['libtcnative-1', 'liblog4j1.2-java', 'libcommons-logging-java']: ensure => held, }
+    package { ['libtcnative-1', 'liblog4j1.2-java', 'libcommons-logging-java']: ensure => held, }
 
-  file { [$tomcat::params::root, $tomcat::params::home, '/etc/tomcat.d/',]:
-    ensure => directory,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0644',
-  }
-
-  file { '/etc/init.d/tomcat':
-    source => 'puppet:///modules/tomcat/tomcat',
-    owner  => 'root',
-    group  => 'root',
-  }
-
-  file { '/usr/sbin/tomcat':
-    ensure => link,
-    target => '/etc/init.d/tomcat',
-    owner  => 'root',
-    group  => 'root',
-  }
-
-  service { "tomcat${version}":
-    ensure  => stopped,
-    pattern => "/var/lib/tomcat${version}",
-    enable  => false,
-    require => Package["tomcat${version}"],
-  }
-
-  exec { 'create tomcat init script links':
-    command => '/usr/sbin/update-rc.d tomcat defaults',
-    creates => '/etc/rc0.d/K20tomcat',
-    require => File['/etc/init.d/tomcat'],
-  }
-
-  if $start_on_boot {
-    exec { 'enable tomcat autostarting':
-      command => '/usr/sbin/update-rc.d tomcat enable',
-      creates => '/etc/rc2.d/S20tomcat',
-      require => [
-        File['/etc/init.d/tomcat'],
-        Exec['create tomcat init script links'],
-      ],
+    File {
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0644',
     }
-  } else {
-    exec { 'disable tomcat autostarting':
-      command => '/usr/sbin/update-rc.d tomcat disable',
-      creates => '/etc/rc2.d/K80tomcat',
-      require => [
-        File['/etc/init.d/tomcat'],
-        Exec['create tomcat init script links'],
-      ],
+        
+
+    file { [$tomcat::params::root, $tomcat::params::home, '/etc/tomcat.d/',]:
+        ensure => directory,
     }
-  }
 
-  profile_d::script { 'CATALINA_HOME.sh':
-    ensure  => present,
-    content => "export CATALINA_HOME=/usr/share/tomcat${version}",
-  }
+    file { '/etc/init.d/tomcat':
+        source => 'puppet:///modules/tomcat/tomcat',
+        mode   => '0755',
+    }
 
-  profile_d::script { 'CATALINA_BASE.sh':
-    ensure  => present,
-    content => 'export CATALINA_BASE=$HOME/tomcat',
-  }
+    file { '/usr/sbin/tomcat':
+        ensure => link,
+        target => '/etc/init.d/tomcat',
+    }
+
+    service { "tomcat${version}":
+        ensure  => stopped,
+        pattern => "/var/lib/tomcat${version}",
+        enable  => false,
+        require => Package["tomcat${version}"],
+    }
+
+    exec { 'create tomcat init script links':
+        command => '/usr/sbin/update-rc.d tomcat defaults',
+        creates => '/etc/rc0.d/K20tomcat',
+        require => File['/etc/init.d/tomcat'],
+    }
+
+    if $start_on_boot {
+        exec { 'enable tomcat autostarting':
+            command => '/usr/sbin/update-rc.d tomcat enable',
+            creates => '/etc/rc2.d/S20tomcat',
+            require => [
+                File['/etc/init.d/tomcat'],
+                Exec['create tomcat init script links'],
+            ],
+        }
+    } else {
+        exec { 'disable tomcat autostarting':
+            command => '/usr/sbin/update-rc.d tomcat disable',
+            creates => '/etc/rc2.d/K80tomcat',
+            require => [
+                File['/etc/init.d/tomcat'],
+                Exec['create tomcat init script links'],
+            ],
+        }
+    }
+
+    profile_d::script { 'CATALINA_HOME.sh':
+        ensure  => present,
+        content => "export CATALINA_HOME=/usr/share/tomcat${version}",
+    }
+
+    profile_d::script { 'CATALINA_BASE.sh':
+        ensure  => present,
+        content => 'export CATALINA_BASE=$HOME/tomcat',
+    }
 }
